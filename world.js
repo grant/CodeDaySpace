f2fd16157bc97030706ad8243a63cff6a01cf564
avasc <alexvas@u.washington.edu>
2014-01-18 14:23:26 -0800

    added actor and world
    world is stubbed

---
+++ world.js
@@ -0,0 +1,17 @@
+
+var actors = new 
+function world() {
+    //initialization: initalize networking class
+}
+
+function update() {
+
+    //update each actor
+}
+
+function pushToServer() {
+    //tell the newtorking class what to push to the server
+}
+
+
+
