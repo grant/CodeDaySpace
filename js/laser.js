8abe7908f99f9fbe7245e04403417b6098794ca7
vovakkk <vladimirkorukov@gmail.com>
2014-01-19 04:29:13 -0800

    Laser damage

--- js/laser.js
+++ js/laser.js
@@ -7,16 +7,28 @@ function Laser(params) {
     this.ydes = params.ydes || 0;
     this.frameLife = params.frameLife;
     var ydiff = self.ydes - (self.y + 0.5 * self.height);
-    var xdiff = self.xdes - (self.x+0.5*self.width);
+    var xdiff = self.xdes - (self.x + 0.5 * self.width);
     var angle = Math.atan2(ydiff, xdiff);
-    this.rot = 90+angle * (180 / Math.PI);
+    this.rot = 90 + angle * (180 / Math.PI);
+    this.dmg = params.damage;
 
     this.vx = this.v * Math.cos(angle);
     this.vy = this.v * Math.sin(angle);
 
-    this.update = function (deltaT, actors){
+    this.update = function (deltaT, actors) {
         self.x += self.vx * deltaT / 10000;
         self.y += self.vy * deltaT / 10000;
     };
 
+    var parentSer = this.serialize;
+    this.serialize = function () {
+        return parentSer() +
+            Helpers.packFloat(this.dmg);
+    };
 }
+
+Laser.deserialize = function (data, ind) {
+    var params = Actor.deserialize(data, ind);
+    params.damage = Helpers.parseFloat(data, ind);
+    return params;
+};
--- js/world.js
+++ js/world.js
@@ -33,6 +33,7 @@ function World() {
             vy: 0,
             maxV: 1500,
             maxA: 1000,
+            health: 200,
             img: 'img/spaceship.png'
         });
         self.addActor(newShip);
@@ -76,6 +77,12 @@ function World() {
                     act.type = actType;
                     acts.push(act);
                     break;
+                case 1:
+                    var act = new Laser(Laser.deserialize(data, ind));
+                    act.userId = uId;
+                    act.type = actType;
+                    acts.push(act);
+                    break;
             }
         }
         return acts;
@@ -83,7 +90,23 @@ function World() {
 
     var deserialize = function (data) {
         var acts = unpackActors(data);
-        acts.forEach(function (act) { act.repaint(); })
+        acts.forEach(function (act) {
+            act.repaint();
+            if (act.type == Actor.LASER) {
+                for (var i = actors.length - 1; i >= 0; i --)
+                    var cact = actors[i];
+                    if (cact.type == Actor.SPACESHIP) {
+                        if ((cact.x - act.x) * (cact.x - act.x) + (cact.y - act.y) * (cact.y - act.y) < 30) {
+                            cact.health -= act.dmg;
+                            if (cact.health < 0) {
+                                cact.dom.remove();
+                                actors.pop(i);
+                            }
+                        }
+                    }
+                });
+            }
+        })
         otherActors.forEach(function (act) { act.dom.remove(); });
         otherActors = acts;
     };
